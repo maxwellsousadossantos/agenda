81ded9d3799fcf0d6cdefe2303780c6224f754ad
maxwellsousadossantos <max.sousadossantos06@gmail.com>
2025-08-27 11:12:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // config.js
-const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-const SUPABASE_KEY = "SUA-CHAVE-ANON-AQUI";
+const SUPABASE_URL = "https://gadcwrmubhdamljwyirc.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZGN3cm11YmhkYW1sand5aXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyOTcwMDcsImV4cCI6MjA3MTg3MzAwN30.r7qi6Wgj5bqw9SDRTiXzkOT0bLcorEiTTFbg60-hkT0";
 const supabaseClient = supabase.createClient(SUPABASE_URL,
 SUPABASE_KEY);
+
