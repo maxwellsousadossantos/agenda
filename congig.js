63b40f08ef01cb95eec0a8533c85c24f3260aeb4
maxwellsousadossantos <max.sousadossantos06@gmail.com>
2025-08-27 09:59:43 -0300

    Add files via upload

---
+++ congig.js
@@ -0,0 +1,5 @@
+// config.js
+const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
+const SUPABASE_KEY = "SUA-CHAVE-ANON-AQUI";
+const supabaseClient = supabase.createClient(SUPABASE_URL,
+SUPABASE_KEY);
